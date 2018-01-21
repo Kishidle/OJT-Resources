a1c326487d898675df2c80d1d8986655cf5d645e
VN <arcaramon@gmail.com>
2018-01-21 20:25:24 +0800

    more stuff

--- (re)learning-android/MPAndroidChartTest/app/src/main/java/com/example/user/mpandroidcharttest/ChartActivity.java
+++ (re)learning-android/MPAndroidChartTest/app/src/main/java/com/example/user/mpandroidcharttest/ChartActivity.java
@@ -2,16 +2,31 @@ package com.example.user.mpandroidcharttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.github.mikephil.charting.charts.PieChart;
 
 public class ChartActivity extends AppCompatActivity {
+
+    private PieChart mPie1;
+    private PieChart mPie2;
+    private Spinner mSpin1;
+    private Spinner mSpin2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
 
-        PieChart pie = (PieChart) findViewById(R.id.piechart);
+        mPie1 = (PieChart) findViewById(R.id.piechart1);
+        mPie2 = (PieChart) findViewById(R.id.piechart2);
+
+        mSpin1 = (Spinner) findViewById(R.id.chart1_spinner);
+        mSpin2 = (Spinner) findViewById(R.id.chart2_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.test_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpin1.setAdapter(adapter);
+
     }
 }
