026dcd91398c54f2d697b1d27a42e11fe953159c
Kishidle <arcaramon@gmail.com>
2018-03-08 22:18:03 +0800

    added buttons, changing of activity

--- (re)learning-android/OtherProject/app/src/main/java/com/example/user/otherproject/MainMenu.java
+++ (re)learning-android/OtherProject/app/src/main/java/com/example/user/otherproject/MainMenu.java
@@ -1,5 +1,6 @@
 package com.example.user.otherproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    //TODO: get database and store, make UI a little bit better/presentable
+
+    private void launchChartActivity(){
+        Intent intent = new Intent(this, ChartActivity.class);
+        startActivity(intent);
     }
 }
